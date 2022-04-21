e4831e922caf759bf7ae4d30f9e54916fde7f81c
nadiamaharani25 <nadiamaharaniii04@gmail.com>
2022-04-21 10:15:49 +0700

    First Push

---
+++ src/config/api.js
@@ -0,0 +1,20 @@
+export const API = () => {
+  const baseUrl =
+    process.env.REACT_APP_SERVER_URL ||
+    'https://demo-backend-deploy.herokuapp.com/api/v1' ||
+    'https://localhost:5001/api/v1';
+
+
+  const executeAPI = async (endpoint, config) => {
+    const response = await fetch(baseUrl + endpoint, config);
+    const data = await response.json();
+    return data;
+  };
+
+  return {
+    get: executeAPI,
+    post: executeAPI,
+    patch: executeAPI,
+    delete: executeAPI,
+  };
+};
